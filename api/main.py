abce0676cb0c28e7c590df3576db7a0811ce5ffc
Gus <gus@Guss-MacBook-Air.local>
2025-12-18 15:10:00 -0300

    add root url

--- api/main.py
+++ api/main.py
@@ -3,6 +3,10 @@ import os
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Fintexa Demo API"}
+
 @app.get("/health")
 def health_check():
     return {"status": "[OK]", "service": "demo-api", "env": os.getenv("ENV", "unknown")}
